85afec551d950fe9aee33f34fa5cd531d625984e
DanielaOrellanaS <deorellana@espe.edu.ec>
2024-10-23 20:28:27 -0500

    modify allowed origins

--- project/settings.py
+++ project/settings.py
@@ -27,10 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CORS_ALLOWED_ORIGINS = [
+""" CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-]
+] """
 
 CORS_ALLOW_ALL_ORIGINS = True 
 

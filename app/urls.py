4c6fd0f449ec6aa992163a0f9afeb457a16db992
DanielaOrellanaS <deorellana@espe.edu.ec>
2024-11-01 13:41:25 -0500

    add new endpoint for profile

--- app/urls.py
+++ app/urls.py
@@ -9,4 +9,5 @@ router.register(r'constructoras', BuilderCompanyViewSet, basename='constructora'
 urlpatterns = [
     path('', include(router.urls)),
     path('login/', AuthViewSet.as_view({'post': 'login'}), name='login'),
+    path('user/profile/', user_profile, name='user_profile'),
 ]
--- app/views.py
+++ app/views.py
@@ -5,6 +5,8 @@ from rest_framework.permissions import AllowAny
 from rest_framework.authtoken.models import Token
 from .models import CommercialAdvisor, BuilderCompany
 from .serializers import ( CommercialAdvisorSerializer, BuilderCompanySerializer, LoginSerializer )
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
 
 class CommercialAdvisorViewSet(viewsets.ModelViewSet):
     queryset = CommercialAdvisor.objects.all()
@@ -30,3 +32,40 @@ class AuthViewSet(viewsets.ViewSet):
                 return Response({'token': token.key}, status=status.HTTP_200_OK)
             return Response({'error': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def user_profile(request):
+    user = request.user
+    if user.is_constructora:
+        builder = user.builder_company
+        profile_data = {
+            "user": {
+                "id": user.id,
+                "email": user.email,
+                "name": user.name,
+                "identification": user.identification,
+            },
+            "incorporation_time": builder.incorporation_time,
+            "legal_representative": builder.legal_representative,
+            "address": builder.address,
+        }
+    elif user.is_asesor:
+        advisor = user.commercial_advisor
+        profile_data = {
+            "user": {
+                "id": user.id,
+                "email": user.email,
+                "name": user.name,
+                "identification": user.identification,
+            },
+            "biography": advisor.biography,
+            "skills": advisor.skills,
+            "reviews": advisor.reviews,
+            "num_sales": advisor.num_sales,
+        }
+    else:
+        return Response({"error": "Tipo de usuario no reconocido."}, status=400)
+
+    return Response(profile_data)
